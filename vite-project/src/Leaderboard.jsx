90241e9e4c3acff646c9fb63fc731096178fd107
allison-truhlar <114596142+allison-truhlar@users.noreply.github.com>
2023-02-07 21:45:40 -0500

    Added conditional styling for the userboard
    Entry turns red if your roll count is in the top three rolls

--- vite-project/src/Leaderboard.jsx
+++ vite-project/src/Leaderboard.jsx
@@ -1,13 +1,27 @@
 import React from "react"
 
 export default function Leaderboard(props){
+    const userPlace = props.topScores.indexOf(props.rollCount)
+    console.log(props.rollCount)
+    console.log(props.topScores)
+    console.log(userPlace)
+    
+    const leaderboardEntries = props.topScores.map((score, index) => {
+        const placeOrder = ["🥇 1st","🥈 2nd","🥉 3rd"]
+        const styles = {
+            color: index===userPlace ? "red" : "#2B283A"
+        }
+        return(
+            <p className="leaderboard-entry" style={styles}key={index}>{placeOrder[index]} place: {score} rolls</p>
+        )
+    })
+
+
     return(
         <div className="leaderboard-container">
-            <h1 className="leaderboard-title">You Won!</h1>
+            <h1 className="leaderboard-title">Tenzies!</h1>
             <h2 className="leaderboard-subtitle">Roll Count Leaderboard</h2>
-            <p className="leaderboard-entry">1st - {props.topScores[0]}</p>
-            {props.topScores[1] && <p className="leaderboard-entry">2nd - {props.topScores[1]}</p>}
-            {props.topScores[2] && <p className="leaderboard-entry">2nd - {props.topScores[2]}</p>}
+            {leaderboardEntries}
         </div>
     )
 }
